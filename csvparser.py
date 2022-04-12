60c55d34f82c1b59a1810bfc55b6ac863230521b
Rasyadan <88817085+Rasyadan@users.noreply.github.com>
2022-04-12 14:03:05 +0700

    Update csvparser.py
    Perbaikan ModularitasFungsi

--- csvparser.py
+++ csvparser.py
@@ -14,27 +14,27 @@ def removen(array):
     return(result)       
 
 
-def parse(array):
+def parse(array,jumlah_kategori): # jumlah_kategori adalah jumlah kategori dalam sebuah file csv
     raw_csv_array = removen(array)
-    result_array = ["" for i in range(6*(arrayLen(raw_csv_array)))]
-    temp_array = ["" for i in range(6)]
+    result_array = ["" for i in range(jumlah_kategori*(arrayLen(raw_csv_array)))]
+    temp_array = ["" for i in range(jumlah_kategori)]
 
     for i in range(0, arrayLen(raw_csv_array)):
         temp_array = split(raw_csv_array[i], ";")
-        for j in range(1,7):
-            result_array[(j-1) + (6*(i))] = temp_array[j-1]
+        for j in range(1,jumlah_kategori+1):
+            result_array[(j-1) + (jumlah_kategori*(i))] = temp_array[j-1]
     
     return(result_array)
 
-def tocsv(array): #Mengubah bentuk array data menjadi bentuk csv
-    csv_array = ["" for i in range(arrayLen(array)//6)]
+def tocsv(array, jumlah_kategori): #Mengubah bentuk array data menjadi bentuk csv
+    csv_array = ["" for i in range(arrayLen(array)//jumlah_kategori)]
     temp_string = ""
-    for i in range(arrayLen(array)//6):
+    for i in range(arrayLen(array)//jumlah_kategori):
         temp_string = ""
-        for j in range(6):
+        for j in range(jumlah_kategori):
             if (j != 5) :
-                temp_string += (array[j + (6*i)] + ";") 
+                temp_string += (array[j + (jumlah_kategori*i)] + ";") 
             else :
-                temp_string += array[j + (6*i)] + "\n"
+                temp_string += array[j + (jumlah_kategori*i)] + "\n"
         csv_array[i] = temp_string
     return(csv_array)
