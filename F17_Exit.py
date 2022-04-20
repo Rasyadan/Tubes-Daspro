0696ca33433447b7f18f4de194dfd35b8496846c
nauravalda <89316388+nauravalda@users.noreply.github.com>
2022-04-20 17:24:20 +0700

    Update F17_Exit.py

--- F17_Exit.py
+++ F17_Exit.py
@@ -1,28 +1,28 @@
 import save
 
 def exit():
-    state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
-    if state!="Y" or state!="y" or state!="N" or state!="n":
-        inputValid=False
-    else:
+    inputValid=False
+    state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+    if state=="Y" or state=="y" or state=="N" or state=="n":
+        inputValid
         if state=="Y" or state=="y":
-                save()
-                exit=True
-                # Pada program utama terdapat while loop yang dijalankan jika boolean exit=False, 
-                # jika exit=True maka program akan berhenti dijalankan 
+            save()
+            exit=True
         else:
-                exit=False
-        return(exit)
-    while not inputValid:    
-        print("Ketik (y/n)")
-        state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
-        if state!="Y" or state!="y" or state!="N" or state!="n":
-            inputValid=False
-        else:
-            if state=="Y" or state=="y":
-                save()
-                exit=True
+            exit=False
+    else:
+        inputValid=False
+        while not inputValid:    
+            print("Ketik (y/n)")
+            state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
+            if state=="Y" or state=="y" or state=="N" or state=="n":
+                inputValid=True
+                if state=="Y" or state=="y":
+                    save()
+                    exit=True
+                else:
+                    exit=False
             else:
-                exit=False
-            return(exit)
+                inputValid=False
+    return(exit)
 
