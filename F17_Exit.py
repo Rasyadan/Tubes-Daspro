b6c0e5676184f1495bf2eccba619846bfb46c503
nauravalda <89316388+nauravalda@users.noreply.github.com>
2022-04-23 18:40:07 +0700

    Update F17_Exit.py

--- F17_Exit.py
+++ F17_Exit.py
@@ -1,28 +1,36 @@
 import save
 
 def exit():
-    inputValid=False
-    state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    if state=="Y" or state=="y" or state=="N" or state=="n":
-        inputValid
-        if state=="Y" or state=="y":
-            save()
-            exit=True
-        else:
+    print("Apakah Anda ingin keluar dari game?")
+    print("1. Ya")
+    print("2. Tidak")
+    state1=int(input("Ketikkan di sini: "))
+    if state1==1 or state1==2:
+        if state1==2:
             exit=False
-    else:
-        inputValid=False
-        while not inputValid:    
-            print("Ketik (y/n)")
-            state=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
-            if state=="Y" or state=="y" or state=="N" or state=="n":
-                inputValid=True
-                if state=="Y" or state=="y":
+        else:
+            inputValid=False
+            state2=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+            if state2=="Y" or state2=="y" or state2=="N" or state2=="n":
+                inputValid
+                if state2=="Y" or state2=="y":
                     save()
                     exit=True
                 else:
-                    exit=False
+                    exit=True
             else:
                 inputValid=False
+                while not inputValid:    
+                    print("Ketik (y/n)")
+                    state2=input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
+                    if state2=="Y" or state2=="y" or state2=="N" or state2=="n":
+                        inputValid=True
+                        if state2=="Y" or state2=="y":
+                            save()
+                            exit=True
+                        else:
+                            exit=True
+                    else:
+                        inputValid=False
     return(exit)
 
