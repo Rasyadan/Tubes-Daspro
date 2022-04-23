899b3da8a9636e87a827e1aa02e2c37b7d5dedb4
nauravalda <89316388+nauravalda@users.noreply.github.com>
2022-04-24 01:13:37 +0700

    Update F04_TambahGame.py

--- F04_TambahGame.py
+++ F04_TambahGame.py
@@ -2,9 +2,9 @@ from lensplit import arrayLength
 
 def isAdmin (data_pengguna):
     if data_pengguna[4]=="admin":
-        True
+        return(True)
     else:
-        False
+        return (False)
 
 def tambahgame(game,data_pengguna):
     if isAdmin(data_pengguna):
@@ -12,11 +12,11 @@ def tambahgame(game,data_pengguna):
         while not inputSelesai:
             nama= input("Masukkan nama game: ")
             kategori = input("Masukkan kategori: ")
-            tahun_rilis= int(input("Masukkan tahun rilis: "))
-            harga=int(input("Masukkan harga: "))
-            stok_awal=int(input("Masukkan stok awal: "))
+            tahun_rilis= input("Masukkan tahun rilis: ")
+            harga=input("Masukkan harga: ")
+            stok_awal=input("Masukkan stok awal: ")
             if (nama!="" and kategori!="" and tahun_rilis!="" and harga!="" and stok_awal!="" ):
-                tambahan=[f"GAME00{arrayLength(game)/6+2}",nama,kategori,tahun_rilis,harga,stok_awal]
+                tambahan=[f"GAME00{round(arrayLength(game)/6)}",nama,kategori,int(tahun_rilis),int(harga),int(stok_awal)]
                 inputSelesai=True
             else:
                 print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
@@ -25,6 +25,7 @@ def tambahgame(game,data_pengguna):
         game = game+tambahan
         return (game)
     else:
-        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
+        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.")
+        print("Mintalah ke administrator untuk melakukan hal tersebut.")
         return (game)
 
