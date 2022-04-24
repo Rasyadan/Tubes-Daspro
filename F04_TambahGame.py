d6a99ade175a6eb72ae3cc437b745675e8cb11d0
nauravalda <89316388+nauravalda@users.noreply.github.com>
2022-04-24 13:50:39 +0700

    Update F04_TambahGame.py

--- F04_TambahGame.py
+++ F04_TambahGame.py
@@ -16,7 +16,16 @@ def tambahgame(game,data_pengguna):
             harga=input("Masukkan harga: ")
             stok_awal=input("Masukkan stok awal: ")
             if (nama!="" and kategori!="" and tahun_rilis!="" and harga!="" and stok_awal!="" ):
-                tambahan=[f"GAME00{round(arrayLength(game)/6)}",nama,kategori,int(tahun_rilis),int(harga),int(stok_awal)]
+                if arrayLength(game)//6<=9:
+                    ID=f"GAME00{arrayLength(game)//6}"
+                elif arrayLength(game)//6>9 and arrayLength(game)//6<=99:
+                    ID=f"GAME0{arrayLength(game)//6}"
+                elif arrayLength(game)//6>99 and arrayLength(game)//6<=999:
+                    ID=f"GAME{arrayLength(game)//6}"
+                else:
+                    print("Penyimpanan data game tidak mencukupi.")
+                    break
+                tambahan=[ID,nama,kategori,int(tahun_rilis),int(harga),int(stok_awal)]
                 inputSelesai=True
             else:
                 print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
