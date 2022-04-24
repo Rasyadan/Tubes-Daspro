9c32463b3dd257b1ee410aa45900d14a752069b6
nauravalda <89316388+nauravalda@users.noreply.github.com>
2022-04-24 14:12:15 +0700

    update comment

--- F03_Login.py
+++ F03_Login.py
@@ -1,10 +1,19 @@
 from lensplit import arrayLength
 
 def login(user):
+    # fungsi login menerima input username, password lalu mereturn data pengguna jika sudah login
+
+    # KAMUS: 
+        # nama,username,password: String
+        # logged_in,found : Boolean
+        # i : integer
+        # data_pengguna: Array
+        # function arrayLength {mengembalikan panjang suatu array}
+    # ALGORITMA:
     username = (input("Masukkan username: "))
     password = (input("Masukkan password: "))
-    logged_in=True
-    found=False
+    logged_in=True # State awal adalah true, jika pengguna tidak berhasil login maka state diganti false
+    found=False # state awal apakah username ditemukan atau tidak
     for i in range (7,arrayLength(user),6): # mengakses username mulai dari user id pertama
         if user[i]==username:
             if user[i+2]==password:
@@ -19,7 +28,7 @@ def login(user):
                     logged_in=True
                     print("=="*24)
                     found=True
-                    return data_pengguna
+                    return data_pengguna # jika berhasil login maka data_pengguna direturn
             else:
                     print("Password Anda salah.")
                     found=True
